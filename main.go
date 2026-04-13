31066543a06edc754494b688017d4fb709d291de
DGonzo01 <127304401+DGonzo01@users.noreply.github.com>
2026-04-13 19:22:30 +0300

    up

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+type Bank struct {
+	Name    string
+	BioFrom int
+	BinTo   int
+}
+
 func main() {
-	fmt.Println("Hello World")
+	bank := Bank{}
+	lanarbank := Bank{Name: "Lunar Bank", BioFrom: 400000, BinTo: 499999}
+	fmt.Println(bank)
+	fmt.Println(lanarbank)
 }
