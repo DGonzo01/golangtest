b3b16b044d58d1223f6bf4fc9708cf90f7a675d2
DGonzo01 <127304401+DGonzo01@users.noreply.github.com>
2026-04-13 19:24:00 +0300

    up

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import "fmt"
 
 type Bank struct {
 	Name    string
-	BioFrom int
+	BinFrom int
 	BinTo   int
 }
 
 func main() {
 	bank := Bank{}
-	lanarbank := Bank{Name: "Lunar Bank", BioFrom: 400000, BinTo: 499999}
+	lanarbank := Bank{Name: "Lunar Bank", BinFrom: 400000, BinTo: 499999}
 	fmt.Println(bank)
 	fmt.Println(lanarbank)
 }
